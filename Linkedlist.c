50a23c94d4cd79b1253e735f72e62876080f85bc
MayBMore <43025864+MayBMore@users.noreply.github.com>
2018-12-06 15:40:53 +0900

    03

--- Linkedlist.c
+++ Linkedlist.c
@@ -1,6 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct linknode {
 	int data;
 	void *next;
 	//struct linked *next;(둘중하나 아무거나 쓰기) 
 } linknd_t;
 
+static linknd_t *list; //linked List 실체
+
+linknd_t* create_node(int value) {
+	linknd_t* ndPtr;
+	
+	//동적 메모리 할당
+	ndPtr = (linknd_t*)malloc(sizeof(linknd_t));
+	if (ndPtr == NULL) {
+		printf("ERROR\n");
+		return NULL;
+	}
+	
+	//정수값저장 
+	ndPtr->data = value;
+	ndPtr->next = NULL;
+	
+	return ndPtr; 
+}
