38faf3b4703909059a9f77a3cdf3fd7e3e87c7d6
MayBMore <43025864+MayBMore@users.noreply.github.com>
2018-12-06 16:35:25 +0900

    08

--- Linkedlist.c
+++ Linkedlist.c
@@ -43,19 +43,25 @@ void addTail(int value) {
 	
 }
 
-void genList(void) {
+void genList() {
 	list = create_node(0);
 	
 	return;
 }
 
-void print_list(void) {
+void print_list(int n) {
 	linknd_t* ndPtr;
+	int i=0;
 
 	ndPtr = (linknd_t*)list->next;
 	
 	while(ndPtr != NULL) {
-		printf("%i\n", ndPtr->data);
+		i++;
+		
+		if(i==n) {
+			printf("%i-th value : %i\n", i, ndPtr->data);
+			break;
+		}
 		ndPtr = ndPtr->next;	
 	}
 }
